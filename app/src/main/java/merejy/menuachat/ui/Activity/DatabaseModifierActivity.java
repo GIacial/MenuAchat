33cd89967f79cfb8775ca09f997bf84a7247e797
Glacial <regnier.jeremy@outlook.fr>
2019-01-02 17:53:51 +0100

    Confort ihm
    permet de cacher plats ou ingredient dans DATABASE_MODIFIER
    Sauvegarde faite a la fermeture de DATABASE_MODIFIER

--- app/src/main/java/merejy/menuachat/ui/Activity/DatabaseModifierActivity.java
+++ app/src/main/java/merejy/menuachat/ui/Activity/DatabaseModifierActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import merejy.menuachat.R;
 import merejy.menuachat.database.Database;
@@ -17,7 +20,7 @@ import merejy.menuachat.ui.ViewAdapter.ChoiceInSortedIngredientListAdapter;
 import merejy.menuachat.ui.ViewAdapter.ChoiceIngredientAdapter;
 import merejy.menuachat.ui.ViewAdapter.ChoicePlatAdapter;
 
-public class DatabaseModifierActivity extends AbstractActivity {
+public class DatabaseModifierActivity extends ActivitySaveOnClose {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,7 @@ public class DatabaseModifierActivity extends AbstractActivity {
 
         final Database database = Database.getDatabase();
 
-        RecyclerView platView = findViewById(R.id.platList);
+        final RecyclerView platView = findViewById(R.id.platList);
         platView.setLayoutManager(new LinearLayoutManager(this));
         platView.setAdapter(new ChoicePlatAdapter(database.getAllPlat(),this,new ChoicePlat_Modifier_OnClickListenerCreator()));
 
@@ -51,6 +54,53 @@ public class DatabaseModifierActivity extends AbstractActivity {
                 }
             }
         });
+
+        final LinearLayout optionCat = findViewById(R.id.optionCat);
+        View platText = findViewById(R.id.text_plat);
+        View ingredientText = findViewById(R.id.text_ingredient);
+        final ImageView platImage = findViewById(R.id.plat_image);
+        final ImageView ingredientImage = findViewById(R.id.ingredient_image);
+
+        View.OnClickListener platHidden = new View.OnClickListener() {
+
+            private boolean hidden = false;
+            @Override
+            public void onClick(View v) {
+                hidden = !hidden;
+                if(hidden){
+                    platView.setVisibility(View.GONE);
+                    platImage.setImageResource(android.R.drawable.arrow_down_float);
+                }
+                else{
+                    platView.setVisibility(View.VISIBLE);
+                    platImage.setImageResource(android.R.drawable.arrow_up_float);
+                }
+            }
+        };
+
+        View.OnClickListener ingredientHidden = new View.OnClickListener() {
+            private boolean hidden = false;
+            @Override
+            public void onClick(View v) {
+                hidden = !hidden;
+
+                if(hidden) {
+                    optionCat.setVisibility(View.GONE);
+                    ingredientView.setVisibility(View.GONE);
+                    ingredientImage.setImageResource(android.R.drawable.arrow_down_float);
+                }
+                else{
+                    optionCat.setVisibility(View.VISIBLE);
+                    ingredientView.setVisibility(View.VISIBLE);
+                    ingredientImage.setImageResource(android.R.drawable.arrow_up_float);
+                }
+            }
+        };
+
+        platText.setOnClickListener(platHidden);
+        ingredientText.setOnClickListener(ingredientHidden);
+        platImage.setOnClickListener(platHidden);
+        ingredientImage.setOnClickListener(ingredientHidden);
 
         Button comfirmer = findViewById(R.id.button_comfirmer);
         comfirmer.setOnClickListener(new View.OnClickListener() {
